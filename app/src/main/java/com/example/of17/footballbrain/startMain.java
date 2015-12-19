f84ed3c92a0c5bc95943955ec384fa9c61e3e58c
ZapriyRoman <zapriy.05@mail.ru>
2015-12-19 23:11:49 +0200

    заглушки для кнопок

--- app/src/main/java/com/example/of17/footballbrain/startMain.java
+++ app/src/main/java/com/example/of17/footballbrain/startMain.java
@@ -2,6 +2,10 @@ package com.example.of17.footballbrain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class startMain extends AppCompatActivity {
 
@@ -9,5 +13,48 @@ public class startMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_main);
+
+        TextView startButton = (TextView)findViewById(R.id.startButton);
+        TextView exitButton = (TextView)findViewById(R.id.exitButton);
+        TextView chooseMyTeamButton = (TextView)findViewById(R.id.addTeam);
+        TextView chooseEnemyTeamButton = (TextView)findViewById(R.id.addEnemyTeam);
+
+        exitButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        startButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "start",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        chooseMyTeamButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "my team",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        chooseEnemyTeamButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "enemy team",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
